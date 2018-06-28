0cb727f8a36790c3a0dba0ab8566a21f4a75b9d3
zakupower <15311049@students.bfu.bg>
2018-06-28 15:42:57 +0300

    added icon

--- src/audio/Main.java
+++ src/audio/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,6 +16,8 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setMinHeight(400);
         primaryStage.setMinWidth(600);
+        primaryStage.getIcons().add(new Image("file:ico.png"));
+
         primaryStage.show();
     }
 
